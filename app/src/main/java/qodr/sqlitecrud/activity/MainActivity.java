d8e14547ab24176a2994b3d542f20873ff8bb03b
Zainal Fahrudin <fnzainal@gmail.com>
2017-02-12 06:43:54 +0700

    add menu github

--- app/src/main/java/qodr/sqlitecrud/activity/MainActivity.java
+++ app/src/main/java/qodr/sqlitecrud/activity/MainActivity.java
@@ -1,8 +1,11 @@
 package qodr.sqlitecrud.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import qodr.sqlitecrud.R;
@@ -15,6 +18,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().setElevation(0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id== R.id.action_github){
+            String gitHubLink = "https://github.com/qodrorid/android-sql-crud";
+
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(gitHubLink));
+            startActivity(i);
+        }
+
+        return super.onOptionsItemSelected(item);
+
     }
 
     public void gotoCreate(View view) {
